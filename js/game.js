621d149e991ea199ed57842166b801c5868500df
André Leandro <andreleandrodecarvalho@gmail.com>
2023-08-02 09:42:14 -0300

    adicionado função para parar o tempo qnd terminar o jogo

--- js/game.js
+++ js/game.js
@@ -28,7 +28,8 @@ const checkEndGame = () => {
     const disabledCards = document.querySelectorAll('.disabled-card');
 
     if (disabledCards.length === 20) {
-        alert ('Parabéns, você conseguiu!')
+        clearInterval(this.loop)
+        alert (`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`)
     }
 }
 
